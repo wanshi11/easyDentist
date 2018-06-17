8ff8a278c2d8d9907ab94e21a2ee6469e6834a9e
wanshi11 <277780875@qq.com>
2018-06-17 11:19:47 +0800

    11

--- easyDentist/easyDentist_core/src/main/java/com/dentist/utils/Constant.java
+++ easyDentist/easyDentist_core/src/main/java/com/dentist/utils/Constant.java
@@ -10,6 +10,18 @@ public class Constant {
   public static final String LOGIN_USER = "LOGIN_USER";
   public static final String LOGIN_MENUPERMITLIST = "LOGIN_MENUPERMITLIST";
   
+  //文章类型
+  public static final String  ARTICLE_NEWS = "ARTICLE_NEWS";//新闻
+  public static final String  ARTICLE_HEALTH = "ARTICLE_HEALTH";//保健知识
+  public static final String  ARTICLE_ACTIVITY = "ARTICLE_ACTIVITY";//活动
+  
+  
+  //留言类型
+  public static final String  LEAVE_MSG_ADVICE = "LEAVE_MSG_ADVICE"; //建议
+  public static final String  LEAVE_MSG_YUYUE = "LEAVE_MSG_YUYUE"; //预约
+  public static final String  LEAVE_MSG_ZIXUN = "LEAVE_MSG_ZIXUN"; //咨询
+  
+  
   
 
 }
