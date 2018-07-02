bf912e188db6bf552923650fd0e79787f44d8a75
wanshi11 <277780875@qq.com>
2018-07-02 23:28:58 +0800

    11

--- easyDentist/easyDentist_web/src/main/java/com/dentist/controller/admin/ArticleController.java
+++ easyDentist/easyDentist_web/src/main/java/com/dentist/controller/admin/ArticleController.java
@@ -1,12 +1,22 @@
 package com.dentist.controller.admin;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.dentist.entity.Article;
+import com.dentist.entity.ArticleExt;
+import com.dentist.entity.Role;
+import com.dentist.entity.User;
 import com.dentist.service.ArticleService;
+import com.dentist.utils.Constant;
 
 @Controller
 @RequestMapping(value = "/admin/article")
@@ -32,6 +42,26 @@ public class ArticleController {
 	public String addArticle(Model model){
 		
 		return "/admin/article/article_add";
+	}
+	
+	
+	/**
+	 * 添加文章
+	 */
+	@RequestMapping(value = "/add",method=RequestMethod.POST)
+	@ResponseBody
+	public String add(Article article,ArticleExt articleExt,HttpServletRequest request){
+		String result = "";
+		
+		    User u = (User)request.getSession().getAttribute(Constant.LOGIN_USER);
+			
+			/*if(num !=0){
+				result = "ADD_SUCCESS";
+			}else{
+				result = "ADD_FAIL";
+			}*/
+	
+		return result;
 	}
 
 }
--- easyDentist/easyDentist_web/src/main/java/com/dentist/controller/admin/KindEditorController.java
+++ easyDentist/easyDentist_web/src/main/java/com/dentist/controller/admin/KindEditorController.java
@@ -4,15 +4,21 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -27,22 +33,34 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.ByteStreams;
 
 @Controller
 @RequestMapping("/kindeditor")
 public class KindEditorController {
 	
+	
+	private static final ObjectMapper objectMapper = new ObjectMapper();
 
+	      /**
+	       * kindeditor 上传文件（图片，文本，视频等）
+	       * @param request
+	       * @param response
+	       * @return
+	       * @throws ServletException
+	       * @throws IOException
+	       * @throws FileUploadException
+	       */
 	      @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
 	      @ResponseBody
 	      public Map<String, Object> fileUpload(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException,
 	              FileUploadException {
 	          ServletContext application = request.getSession().getServletContext();
-	          String savePath = application.getRealPath("/") + "images/";
+	          String savePath = application.getRealPath("/") + "kindeditor_files/";
 	 
 	          // 文件保存目录URL
-	          String saveUrl = request.getContextPath() + "/images/";
+	          String saveUrl = request.getContextPath() + "/kindeditor_files/";
 	  
 	          // 定义允许上传的文件扩展名
 	          HashMap<String, String> extMap = new HashMap<String, String>();
@@ -62,7 +80,8 @@ public class KindEditorController {
 	          // 检查目录
 	          File uploadDir = new File(savePath);
 	          if (!uploadDir.isDirectory()) {
-	              return getError("上传目录不存在。");
+	        	  uploadDir.mkdir();
+//	              return getError("上传目录不存在。");
 	          }
 	          // 检查目录写权限
 	          if (!uploadDir.canWrite()) {
@@ -96,7 +115,6 @@ public class KindEditorController {
 	         ServletFileUpload upload = new ServletFileUpload(factory);
 	         upload.setHeaderEncoding("UTF-8");
 	 
-	 
 	         MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
 	         
 	         Iterator item = multipartRequest.getFileNames();
@@ -106,26 +124,20 @@ public class KindEditorController {
 	 
 	             MultipartFile file = multipartRequest.getFile(fileName);
 	 
-	 
 	             // 检查文件大小
-	 
 	             if (file.getSize() > maxSize) {
-	 
 	                 return getError("上传文件大小超过限制。");
-	 
 	             }
 	 
 	             // 检查扩展名
-	 
 	             String fileExt = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1).toLowerCase();
 	 
 	             if (!Arrays. asList(extMap.get(dirName).split(",")).contains(fileExt)) {
 	                 return getError("上传文件扩展名是不允许的扩展名。\n只允许"
 	                         + extMap.get(dirName) + "格式。");
-	 
 	             }
+	             
 	             SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
-	 
 	             String newFileName = df.format(new Date()) + "_" + new Random().nextInt(1000) + "." + fileExt;
 	 
 	             try {
@@ -135,9 +147,7 @@ public class KindEditorController {
 	             ByteStreams.copy(file.getInputStream(), new FileOutputStream(uploadedFile));
 	 
 	             } catch (Exception e) {
-	 
 	                 return getError("上传文件失败。");
-	 
 	             }
 	             Map<String, Object> msg = new HashMap<String, Object>();
 	             msg.put("error", 0);
@@ -155,4 +165,178 @@ public class KindEditorController {
 	    	           return msg;
 	    	       }
 	      
-}
+	      
+	      /**
+	 	  * 文件空间
+	 	  * @param request {@link HttpServletRequest}
+	 	  * @param response {@link HttpServletResponse}
+	 	  * @return json
+	 	  */
+	@RequestMapping(value = "/fileManager", method = RequestMethod.POST)
+	public void fileManager(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		ServletContext application = request.getSession().getServletContext();
+		ServletOutputStream out = response.getOutputStream();
+		// 根目录路径，可以指定绝对路径，比如 /var/www/attached/
+		String rootPath = application.getRealPath("/") + "kindeditor_files/";
+		// 根目录URL，可以指定绝对路径，比如 http://www.yoursite.com/attached/
+		String rootUrl = request.getContextPath() + "/kindeditor_files/";
+		// 图片扩展名
+		String[] fileTypes = new String[] { "gif", "jpg", "jpeg", "png", "bmp" };
+
+		String dirName = request.getParameter("dir");
+		if (dirName != null) {
+			if (!Arrays.<String> asList(
+					new String[] { "image", "flash", "media", "file" })
+					.contains(dirName)) {
+				out.println("Invalid Directory name.");
+				return;
+			}
+			rootPath += dirName + "/";
+			rootUrl += dirName + "/";
+			File saveDirFile = new File(rootPath);
+			if (!saveDirFile.exists()) {
+				saveDirFile.mkdirs();
+			}
+		}
+		// 根据path参数，设置各路径和URL
+		String path = request.getParameter("path") != null ? request
+				.getParameter("path") : "";
+		String currentPath = rootPath + path;
+		String currentUrl = rootUrl + path;
+		String currentDirPath = path;
+		String moveupDirPath = "";
+		if (!"".equals(path)) {
+			String str = currentDirPath.substring(0,
+					currentDirPath.length() - 1);
+			moveupDirPath = str.lastIndexOf("/") >= 0 ? str.substring(0,
+					str.lastIndexOf("/") + 1) : "";
+		}
+
+		// 排序形式，name or size or type
+		String order = request.getParameter("order") != null ? request
+				.getParameter("order").toLowerCase() : "name";
+
+		// 不允许使用..移动到上一级目录
+		if (path.indexOf("..") >= 0) {
+			out.println("Access is not allowed.");
+			return;
+		}
+		// 最后一个字符不是/
+		if (!"".equals(path) && !path.endsWith("/")) {
+			out.println("Parameter is not valid.");
+			return;
+		}
+		// 目录不存在或不是目录
+		File currentPathFile = new File(currentPath);
+		if (!currentPathFile.isDirectory()) {
+			out.println("Directory does not exist.");
+			return;
+		}
+		// 遍历目录取的文件信息
+		List<Hashtable> fileList = new ArrayList<Hashtable>();
+		if (currentPathFile.listFiles() != null) {
+			for (File file : currentPathFile.listFiles()) {
+				Hashtable<String, Object> hash = new Hashtable<String, Object>();
+				String fileName = file.getName();
+				if (file.isDirectory()) {
+					hash.put("is_dir", true);
+					hash.put("has_file", (file.listFiles() != null));
+					hash.put("filesize", 0L);
+					hash.put("is_photo", false);
+					hash.put("filetype", "");
+				} else if (file.isFile()) {
+					String fileExt = fileName.substring(
+							fileName.lastIndexOf(".") + 1).toLowerCase();
+					hash.put("is_dir", false);
+					hash.put("has_file", false);
+					hash.put("filesize", file.length());
+					hash.put("is_photo", Arrays.<String> asList(fileTypes)
+							.contains(fileExt));
+					hash.put("filetype", fileExt);
+				}
+				hash.put("filename", fileName);
+				hash.put("datetime",
+						new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(file
+								.lastModified()));
+				fileList.add(hash);
+			}
+		}
+
+		if ("size".equals(order)) {
+			Collections.sort(fileList, new SizeComparator());
+		} else if ("type".equals(order)) {
+			Collections.sort(fileList, new TypeComparator());
+		} else {
+			Collections.sort(fileList, new NameComparator());
+		}
+		Map<String, Object> msg = new HashMap<String, Object>();
+		msg.put("moveup_dir_path", moveupDirPath);
+		msg.put("current_dir_path", currentDirPath);
+		msg.put("current_url", currentUrl);
+		msg.put("total_count", fileList.size());
+		msg.put("file_list", fileList);
+		response.setContentType("application/json; charset=UTF-8");
+		String msgStr = objectMapper.writeValueAsString(msg);
+		out.println(msgStr);
+	}
+
+	class NameComparator implements Comparator {
+		public int compare(Object a, Object b) {
+			Hashtable hashA = (Hashtable) a;
+			Hashtable hashB = (Hashtable) b;
+			if (((Boolean) hashA.get("is_dir"))
+					&& !((Boolean) hashB.get("is_dir"))) {
+				return -1;
+			} else if (!((Boolean) hashA.get("is_dir"))
+					&& ((Boolean) hashB.get("is_dir"))) {
+				return 1;
+			} else {
+				return ((String) hashA.get("filename"))
+						.compareTo((String) hashB.get("filename"));
+			}
+		}
+	}
+
+	class SizeComparator implements Comparator {
+		public int compare(Object a, Object b) {
+			Hashtable hashA = (Hashtable) a;
+			Hashtable hashB = (Hashtable) b;
+			if (((Boolean) hashA.get("is_dir"))
+					&& !((Boolean) hashB.get("is_dir"))) {
+				return -1;
+			} else if (!((Boolean) hashA.get("is_dir"))
+					&& ((Boolean) hashB.get("is_dir"))) {
+				return 1;
+			} else {
+				if (((Long) hashA.get("filesize")) > ((Long) hashB
+						.get("filesize"))) {
+					return 1;
+				} else if (((Long) hashA.get("filesize")) < ((Long) hashB
+						.get("filesize"))) {
+					return -1;
+				} else {
+					return 0;
+				}
+			}
+		}
+	}
+
+	class TypeComparator implements Comparator {
+		public int compare(Object a, Object b) {
+			Hashtable hashA = (Hashtable) a;
+			Hashtable hashB = (Hashtable) b;
+			if (((Boolean) hashA.get("is_dir"))
+					&& !((Boolean) hashB.get("is_dir"))) {
+				return -1;
+			} else if (!((Boolean) hashA.get("is_dir"))
+					&& ((Boolean) hashB.get("is_dir"))) {
+				return 1;
+			} else {
+				return ((String) hashA.get("filetype"))
+						.compareTo((String) hashB.get("filetype"));
+			}
+		}
+	}
+} 
