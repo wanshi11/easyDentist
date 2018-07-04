5924c5498daebdad3f4d80392376617336acee2d
wanshi11 <277780875@qq.com>
2018-07-04 16:59:33 +0800

    11

--- easyDentist/easyDentist_web/src/main/java/com/dentist/controller/admin/ArticleController.java
+++ easyDentist/easyDentist_web/src/main/java/com/dentist/controller/admin/ArticleController.java
@@ -1,22 +1,30 @@
 package com.dentist.controller.admin;
 
-import java.util.Date;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dentist.entity.Article;
 import com.dentist.entity.ArticleExt;
-import com.dentist.entity.Role;
 import com.dentist.entity.User;
 import com.dentist.service.ArticleService;
 import com.dentist.utils.Constant;
+import com.dentist.utils.DateUtil;
+import com.dentist.utils.JsonUtils;
 
 @Controller
 @RequestMapping(value = "/admin/article")
@@ -62,6 +70,42 @@ public class ArticleController {
 			}*/
 	
 		return result;
+	}
+	
+	
+	/**
+	 * 上传文章中的缩略图
+	 * @return
+	 */
+	@RequestMapping(value = "/uploadThumbnail",method=RequestMethod.POST)
+	@ResponseBody
+	public String uploadThumbnail(@RequestParam MultipartFile myfile,HttpServletRequest request){
+		
+        Map<String,Object> map= new HashMap<String,Object>();
+        if(myfile.isEmpty()){
+             map.put( "error", "error");
+             map.put( "msg", "上传文件不能为空" );
+       } else{
+             String originalFilename=myfile.getOriginalFilename();
+             String fileBaseName=FilenameUtils.getBaseName(originalFilename);
+             String floderName=fileBaseName+"_" +DateUtil.getNowDate();
+              try{
+                   
+                   String genePicPath=request.getSession().getServletContext().getRealPath("/upload/" +floderName);
+                    //把上传的图片放到服务器的文件夹下
+                   FileUtils.copyInputStreamToFile(myfile.getInputStream(), new File(genePicPath,originalFilename));
+                    //coding
+                   map.put( "error", "上传成功！");
+                   map.put( "imgurl", "1111");
+                   
+             } catch (Exception e) {
+                   map.put( "error", "error");
+                   map.put( "msg",e.getMessage());
+                   
+             }
+       }
+       String result= JsonUtils.objectToString(map);
+        return result;
 	}
 
 }
