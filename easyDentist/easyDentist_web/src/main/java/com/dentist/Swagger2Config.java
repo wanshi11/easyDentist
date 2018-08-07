e0c87f610a504d6e9a1e553fdd31ca406a053d51
wanshi11 <277780875@qq.com>
2018-08-07 16:55:02 +0800

    11

--- easyDentist/easyDentist_web/src/main/java/com/dentist/Swagger2Config.java
+++ easyDentist/easyDentist_web/src/main/java/com/dentist/Swagger2Config.java
@@ -1,4 +1,4 @@
-package com.dentist;
+/*package com.dentist;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,38 +12,39 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-/**
+*//**
  * 
  * @author xyj
  *
- */
+ *//*
 
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
-	//swagger2的配置文件，这里可以配置swagger2的一些基本的内容，比如扫描的包等等
+	//swagger2鐨勯厤缃枃浠讹紝杩欓噷鍙互閰嶇疆swagger2鐨勪竴浜涘熀鏈殑鍐呭锛屾瘮濡傛壂鎻忕殑鍖呯瓑绛�
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                //为当前包路径
+                //涓哄綋鍓嶅寘璺緞
                 .apis(RequestHandlerSelectors.basePackage("com.dentist.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
-  //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+  //鏋勫缓 api鏂囨。鐨勮缁嗕俊鎭嚱鏁�娉ㄦ剰杩欓噷鐨勬敞瑙ｅ紩鐢ㄧ殑鏄摢涓�
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                 //页面标题
-                .title("SpringBoot集成Swagger2在线API文档")
-                //描述
-                .description("restful风格的API")
+                 //椤甸潰鏍囬
+                .title("SpringBoot闆嗘垚Swagger2鍦ㄧ嚎API鏂囨。")
+                //鎻忚堪
+                .description("restful椋庢牸鐨凙PI")
                 //.termsOfServiceUrl(termsOfServiceUrl)
-                //版本
+                //鐗堟湰
                 .version("1.0")
                 .build();
     }
  
  
 }
+*/
