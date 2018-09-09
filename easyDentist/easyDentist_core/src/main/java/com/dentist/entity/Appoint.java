7e5bd259c218ba82d8ac2287b77a49c11acce7e9
wanshi11 <277780875@qq.com>
2018-09-09 23:18:26 +0800

    11

---
+++ easyDentist/easyDentist_core/src/main/java/com/dentist/entity/Appoint.java
@@ -0,0 +1,135 @@
+package com.dentist.entity;
+
+import java.util.Date;
+
+public class Appoint {
+    private Integer id;
+
+    private String patientname;
+
+    private String phone;
+
+    private String clinic;
+
+    private String messagecontent;
+
+    private String messagetype;
+
+    private String status;
+
+    private String doctorname;
+
+    private Date appointdate;
+
+    private String remark;
+
+    private Integer operatorid;
+
+    private Date createtime;
+
+    private Date updatetime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPatientname() {
+        return patientname;
+    }
+
+    public void setPatientname(String patientname) {
+        this.patientname = patientname == null ? null : patientname.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getClinic() {
+        return clinic;
+    }
+
+    public void setClinic(String clinic) {
+        this.clinic = clinic == null ? null : clinic.trim();
+    }
+
+    public String getMessagecontent() {
+        return messagecontent;
+    }
+
+    public void setMessagecontent(String messagecontent) {
+        this.messagecontent = messagecontent == null ? null : messagecontent.trim();
+    }
+
+    public String getMessagetype() {
+        return messagetype;
+    }
+
+    public void setMessagetype(String messagetype) {
+        this.messagetype = messagetype == null ? null : messagetype.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getDoctorname() {
+        return doctorname;
+    }
+
+    public void setDoctorname(String doctorname) {
+        this.doctorname = doctorname == null ? null : doctorname.trim();
+    }
+
+    public Date getAppointdate() {
+        return appointdate;
+    }
+
+    public void setAppointdate(Date appointdate) {
+        this.appointdate = appointdate;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Integer getOperatorid() {
+        return operatorid;
+    }
+
+    public void setOperatorid(Integer operatorid) {
+        this.operatorid = operatorid;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+}
---
+++ easyDentist/easyDentist_core/src/main/java/com/dentist/mapper/AppointMapper.java
@@ -0,0 +1,30 @@
+package com.dentist.mapper;
+
+import com.dentist.entity.Appoint;
+import com.dentist.entity.AppointExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface AppointMapper {
+    int countByExample(AppointExample example);
+
+    int deleteByExample(AppointExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Appoint record);
+
+    int insertSelective(Appoint record);
+
+    List<Appoint> selectByExample(AppointExample example);
+
+    Appoint selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Appoint record, @Param("example") AppointExample example);
+
+    int updateByExample(@Param("record") Appoint record, @Param("example") AppointExample example);
+
+    int updateByPrimaryKeySelective(Appoint record);
+
+    int updateByPrimaryKey(Appoint record);
+}
