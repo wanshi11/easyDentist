badcac5e0d85b96a82ef420c82d7c4a9efceed69
wanshi11 <277780875@qq.com>
2018-09-16 22:10:41 +0800

    11

---
+++ easyDentist/easyDentist_web/target/classes/public/js/muqruujs.js
@@ -0,0 +1,17 @@
+var oMarquee = document.getElementById("mq"); //滚动对象
+var iLineHeight = 125; //单行高度，像素
+var iLineCount = 4; //实际行数
+var iScrollAmount = 5; //每次滚动高度，像素
+function run() {
+	oMarquee.scrollTop += iScrollAmount;
+	if (oMarquee.scrollTop == iLineCount * iLineHeight)
+		oMarquee.scrollTop = 0;
+	if (oMarquee.scrollTop % iLineHeight == 0) {
+		console.log(11);
+		window.setTimeout("run()", 4500);
+	} else {
+		window.setTimeout("run()", 15);
+	}
+}
+oMarquee.innerHTML += oMarquee.innerHTML;
+window.setTimeout("run()", 4500);
