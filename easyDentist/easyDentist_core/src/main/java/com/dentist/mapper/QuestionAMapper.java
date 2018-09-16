7f6a4515a9d2ba36ed6ac227916b557320fad64f
wanshi11 <277780875@qq.com>
2018-09-16 22:24:33 +0800

    11

---
+++ easyDentist/easyDentist_core/src/main/java/com/dentist/mapper/QuestionAMapper.java
@@ -0,0 +1,30 @@
+package com.dentist.mapper;
+
+import com.dentist.entity.QuestionA;
+import com.dentist.entity.QuestionAExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface QuestionAMapper {
+    int countByExample(QuestionAExample example);
+
+    int deleteByExample(QuestionAExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(QuestionA record);
+
+    int insertSelective(QuestionA record);
+
+    List<QuestionA> selectByExample(QuestionAExample example);
+
+    QuestionA selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") QuestionA record, @Param("example") QuestionAExample example);
+
+    int updateByExample(@Param("record") QuestionA record, @Param("example") QuestionAExample example);
+
+    int updateByPrimaryKeySelective(QuestionA record);
+
+    int updateByPrimaryKey(QuestionA record);
+}
