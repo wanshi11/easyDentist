1b4979927b56ce68b8b8a9f9231632d9c4997da3
wanshi11 <277780875@qq.com>
2018-09-18 22:43:22 +0800

    11

--- db/db_new_dentist_20180918.sql
+++ db/db_new_dentist_20180918.sql
@@ -10,13 +10,13 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-09-18 17:27:29
+Date: 2018-09-18 22:43:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for t_appointment
+-- Table structure for `t_appointment`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_appointment`;
 CREATE TABLE `t_appointment` (
@@ -29,6 +29,7 @@ CREATE TABLE `t_appointment` (
   `status` varchar(20) NOT NULL COMMENT '预约状态',
   `doctorName` varchar(10) DEFAULT NULL COMMENT '预约医生姓名',
   `appointDate` date DEFAULT NULL COMMENT '约定时间',
+  `searchCode` varchar(15) DEFAULT NULL COMMENT '查询码',
   `remark` varchar(1000) DEFAULT NULL COMMENT '回访备注',
   `operatorId` int(11) DEFAULT NULL COMMENT '操作员ID',
   `createTime` datetime NOT NULL COMMENT '留言时间',
@@ -39,11 +40,11 @@ CREATE TABLE `t_appointment` (
 -- ----------------------------
 -- Records of t_appointment
 -- ----------------------------
-INSERT INTO `t_appointment` VALUES ('1', '张三', '13313131313', 'CLINIC_YM', 'LEAVE_MSG_YUYUE', 'LEAVE_MSG_YUYUE', 'WAITING_REPLY', '邓医生', '2018-09-04', null, null, '2018-09-18 13:42:50', null);
-INSERT INTO `t_appointment` VALUES ('2', '李四', '13513131313', 'CLINIC_LL', '111111111111', 'LEAVE_MSG_ZIXUN', 'WAITING_REPLY', '邓医生', '2018-09-26', null, null, '2018-09-03 17:20:48', null);
+INSERT INTO `t_appointment` VALUES ('1', '张三', '13313131313', 'CLINIC_YM', 'LEAVE_MSG_YUYUE', 'LEAVE_MSG_YUYUE', 'WAITING_REPLY', '邓医生', '2018-09-04', null, null, null, '2018-09-18 13:42:50', null);
+INSERT INTO `t_appointment` VALUES ('2', '李四', '13513131313', 'CLINIC_LL', '111111111111', 'LEAVE_MSG_ZIXUN', 'WAITING_REPLY', '邓医生', '2018-09-26', null, null, null, '2018-09-03 17:20:48', null);
 
 -- ----------------------------
--- Table structure for t_article
+-- Table structure for `t_article`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_article`;
 CREATE TABLE `t_article` (
@@ -69,7 +70,7 @@ INSERT INTO `t_article` VALUES ('20', '555555', '555555555', 'ARTICLE_HEALTH', '
 INSERT INTO `t_article` VALUES ('23', 'Test', '测试', 'ARTICLE_NEWS', '2018-07-11 21:03:24', '《阳光灿烂的日子》，改编自王朔的小说《动物凶猛》，1994年最受中国人关注的一部国产电影，姜文的导演处女作。', '1', '1', '1', '/Dentist_UPLOAD/image/art_thumbnail/20180711210231_4.png');
 
 -- ----------------------------
--- Table structure for t_article_ext
+-- Table structure for `t_article_ext`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_article_ext`;
 CREATE TABLE `t_article_ext` (
@@ -89,7 +90,7 @@ INSERT INTO `t_article_ext` VALUES ('14', '20', '<div align=\"center\"><p><stron
 INSERT INTO `t_article_ext` VALUES ('17', '23', '<div align=\"center\">\r\n	<p>\r\n		<strong><span style=\"font-size:14px;color:#337FE5;\">阳光灿烂的日志</span></strong>\r\n	</p>\r\n	<p align=\"left\">\r\n		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\r\n	</p>\r\n	<p align=\"left\">\r\n		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\"><span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\">《阳光灿烂的日子》，改编自王朔的小说《动物凶猛》，1994年最受中国人关注的一部国产电影，姜文的导演处女作，年仅16岁的主演夏雨凭借影片中的马小军一角获得当年的威尼斯影帝的称号。</span><span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\">在这些光环的笼罩下，作为一个影迷，我很难不对它产生期盼。因此产生的后果就是，看完之后我感到有些失落。我并没有获得想像中的快感，或者共鸣，或者感动。而至今还留在记忆中挥之不去的，是它始终昏黄的色调，是它弥漫期间的荷尔蒙的气味，是马小军或者忧愁或者躁动的情绪。</span><br />\r\n</span>\r\n	</p>\r\n	<p align=\"left\">\r\n		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\">这大概就是我第一次看完后说不出它的个中滋味的原因了吧，记忆之中我并没有如此的疯狂过，青春飞扬的时刻好像总是伴随着安静的影子，躁动不安的心灵也掩盖其中。但是看着他们在一齐疯狂的打闹，看着他们骑着自行车在街头巷尾狂奔拍人，看着马小军为了证明自己从高高的烟囱中跳下来满脸煤黑还嘿嘿大笑，看着他疯狂的爱上米兰后因为她而疯狂。我感到的是体内的一种性格的缺失。正因为我没有这些记忆和经历，我体内那些本该被青春释放的激情躁动的因子被埋到了最深处，以至于自己都以为它们并不存在。等到类似的电影情节才将它们从沉睡中唤醒。然后，那种欲探求的渴望和未经历的遗憾会随着起初的不熟悉慢慢的弥散开来，而在记忆中留下一道别样的风景线，一副被涂抹成为金色的画卷，一段沁人心脾的乐曲。</span>\r\n	</p>\r\n	<p align=\"left\">\r\n		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\">“北京，变化这么快！20年的功夫，它已经成为了一个现代化的城市，我几乎从中找不到任何记忆里的东西。事实上，这种变化已经破坏了我的记忆，使我分不清幻觉和真实。我的故事总是发生在夏天，炎热的气候使人们裸露的更多，也更难以掩饰心中的欲望。那时候好像永远是夏天，太阳总是有空伴随着我，阳光充足，太亮了，使我眼前一阵阵发黑……”</span>\r\n	</p>\r\n	<p align=\"center\">\r\n		<span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\"></span><img src=\"/Dentist_UPLOAD/kindeditor_files/image/20180711/20180711205819_132.png\" alt=\"\" />\r\n	</p>\r\n	<p align=\"left\">\r\n		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\">马小军生活的时代在我看来遥不可及，但是同属于青春的朦胧气息却跨越了时空精确的传到达了我身边。那种摸不着抓不紧道不清却又确确实实困扰着我们的感觉，往往让人无从喧泄，只能流露。有些人紧抓着它不放，不厌其烦的无限放大，病态般的沉溺其中，这在当今众多所谓的青春小说中随处可见。也有些人始终渴望着强硬，他们不愿意被这种前所未有的忧伤所困扰，可又确实无法摆脱，于是他们便用相对叛逆的方式来证明自己的态度，证明自己的存在，寻找自己的出口。马小军便是这样的一类人。</span>\r\n	</p>\r\n	<p align=\"left\">\r\n		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\">他无法被困在那个他看来死气沉沉的教室，那个束缚自己激情的地方完全有悖于他的风格，于是他选取逃课。从一个屋顶转到另一个屋顶，从一间房溜到另一间房，这样的行为在他看来并没有任何感情上的色彩，他但是在本能的猎取属于他人空间中的新奇。而近乎顽童的本色看起来也让他乐在其中。</span>\r\n	</p>\r\n	<p align=\"left\">\r\n		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\">这种乐趣一向单纯的持续着，直到他在某间房中看到了米兰的照片。一个漂亮略显丰满成熟的女孩子，自此闯进了他的生活，也打破了他内心的平衡。</span>\r\n	</p>\r\n	<p align=\"left\">\r\n		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <span style=\"color:#222222;font-family:Arial;font-size:14px;font-style:normal;font-weight:400;background-color:#FFFFFF;\">第一次在房间中碰到米兰的时候，姜文用一种窥探的视角直观的精妙的展示了马小军当时复杂细微的内心，趴在床下的他迎接的是一种更为刺激的心理向他发出的挑战，这让蠢蠢欲动的他感到有一丝疑惧却又兴奋不已，空气中开始弥漫淡淡的荷尔蒙的气息。当时的他可能不会想到，最终的他却无可奈何的败下阵来。</span>\r\n	</p>\r\n	<p>\r\n		<br />\r\n	</p>\r\n</div>', '20');
 
 -- ----------------------------
--- Table structure for t_banner
+-- Table structure for `t_banner`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_banner`;
 CREATE TABLE `t_banner` (
@@ -133,7 +134,7 @@ INSERT INTO `t_banner` VALUES ('34', '马连洼店', 'BANNER_ENVIRONMENT', '/Den
 INSERT INTO `t_banner` VALUES ('35', '马连洼前台', 'BANNER_ENVIRONMENT', '/Dentist_UPLOAD/image/banner/20180716230252_马连洼前台.png', '1', '');
 
 -- ----------------------------
--- Table structure for t_config
+-- Table structure for `t_config`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_config`;
 CREATE TABLE `t_config` (
@@ -151,7 +152,7 @@ CREATE TABLE `t_config` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for t_doctor
+-- Table structure for `t_doctor`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_doctor`;
 CREATE TABLE `t_doctor` (
@@ -177,7 +178,7 @@ INSERT INTO `t_doctor` VALUES ('2', '张三', '', '', '修复科主任', '123#11
 INSERT INTO `t_doctor` VALUES ('3', '晓红', '', '', '牙齿治疗顾问', '111#222#333#44#', 'PRACTICE_PHYSICIAN', '牙齿美白，根管治疗等', '/Dentist_UPLOAD/image/doctor_pic/20180812184035_xzj9.jpg', '/Dentist_UPLOAD/image/doctor_inchphoto/20180812184028_xzj9.jpg', '2018-08-12 18:40:38');
 
 -- ----------------------------
--- Table structure for t_menu
+-- Table structure for `t_menu`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_menu`;
 CREATE TABLE `t_menu` (
@@ -212,7 +213,7 @@ INSERT INTO `t_menu` VALUES ('16', '患者中心', '患者管理', null, null, n
 INSERT INTO `t_menu` VALUES ('17', '预约管理', '预约', '16', '/admin/appoint/toAppoints', null, '2018-09-18 09:59:42', '1');
 
 -- ----------------------------
--- Table structure for t_qa
+-- Table structure for `t_qa`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_qa`;
 CREATE TABLE `t_qa` (
@@ -233,7 +234,7 @@ INSERT INTO `t_qa` VALUES ('4', '11', '11333333333333333333333333333333', '1', '
 INSERT INTO `t_qa` VALUES ('5', '333333333333？？', '333333333333333333333333333333333333333333333333333', '1', '2018-09-17');
 
 -- ----------------------------
--- Table structure for t_role
+-- Table structure for `t_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_role`;
 CREATE TABLE `t_role` (
@@ -252,7 +253,7 @@ INSERT INTO `t_role` VALUES ('1', 'ADMIN', '超级管理员', '2018-07-10 00:15:
 INSERT INTO `t_role` VALUES ('3', '文章管理员', '文章管理', '2018-06-25 23:40:15', '1');
 
 -- ----------------------------
--- Table structure for t_role_menu
+-- Table structure for `t_role_menu`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_role_menu`;
 CREATE TABLE `t_role_menu` (
@@ -283,7 +284,7 @@ INSERT INTO `t_role_menu` VALUES ('29', '3', '2', '2018-08-12 18:45:32', '1');
 INSERT INTO `t_role_menu` VALUES ('30', '3', '11', '2018-08-12 18:45:32', '1');
 
 -- ----------------------------
--- Table structure for t_user
+-- Table structure for `t_user`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user` (
@@ -317,7 +318,7 @@ INSERT INTO `t_user` VALUES ('29', '李四', '李四', '96e79218965eb72c92a549dd
 INSERT INTO `t_user` VALUES ('30', '李四', 'lisi', '96e79218965eb72c92a549dd5a330112', '13300000000', '1', '1111', '2018-07-27 23:14:16', '192.168.1.5', '0');
 
 -- ----------------------------
--- Table structure for t_user_role
+-- Table structure for `t_user_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user_role`;
 CREATE TABLE `t_user_role` (
