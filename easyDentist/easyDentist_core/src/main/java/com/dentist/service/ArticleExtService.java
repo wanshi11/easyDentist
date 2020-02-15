76967b7956c5ae6593e08859a3a51bb0b7bb4759
wanshi11 <277780875@qq.com>
2020-02-15 22:47:01 +0800

    delete

--- easyDentist/easyDentist_core/src/main/java/com/dentist/service/ArticleExtService.java
+++
@@ -1,9 +0,0 @@
-package com.dentist.service;
-
-import com.dentist.entity.ArticleExt;
-
-public interface ArticleExtService  extends IBaseService<ArticleExt>{
-
-	ArticleExt queryArticleExtByarticleId(int id);
-
-}
